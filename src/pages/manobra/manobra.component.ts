85356cfc339294dbbfd2dd4555ee78080b725697
Luan Alves da Silva <luanalvessx@gmail.com>
2019-10-24 10:57:02 -0300

    Guaxinim says: Alterado Validador Efika

--- src/pages/manobra/manobra.component.ts
+++ src/pages/manobra/manobra.component.ts
@@ -65,7 +65,7 @@ export class ManobraComponent extends SuperComponentService implements OnInit {
                                         if (_resposta.output.state === "EXCEPTION") {
                                             this.loading(false);
                                             clearInterval(rqSi);
-                                            super.showAlert("Ops, Aconteceu algo", super.makeexceptionmessage(_resposta.output.exceptionMessage + " Para avaliação de manobra, ligar no CO", this.holderService.instancia));
+                                            super.showAlert("DSLAM NÃO SUPORTADO", super.makeexceptionmessage(_resposta.output.exceptionMessage + " Para avaliação de manobra, ligar no CO", this.holderService.instancia));
                                         } else {
                                             this.holderService.validManobra = _resposta.output.resposta;
                                             this.disablemanobraform = true;
